a163c78c75c6e9abf914aaeba58e3f1fdb4cc7de
Andi Yusuf Sinaga <andi@Andis-MacBook-Pro.local>
2018-04-24 17:24:12 +0700

    test

---
+++ components/Slider.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <slide v-for="(slide, i) in dataContent" :key="i">
+            <img :src="slide">
+        </slide>
+    </div>
+</template>
+
+<script>
+import Carousel3d from 'vue-carousel-3d'
+export default {
+  props:['dataContent'],
+  name:'slider',
+  components:{
+    'slides': Carousel3d.Slides,
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  
+ // router: {
+ // base: '/tvrims/'
+    // base:'/tvr/tvrims/dist'
+  // },
+ modules: [
+  '@nuxtjs/axios',
+  ],
+
+  head: {
+    title: 'tvr-iims',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    script: [
+      { src:'https://use.fontawesome.com/releases/v5.0.9/js/all.js'}
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Rajdhani:400,500' }
+    ]
+  },
+
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  plugins:[
+    { src: '~/plugins/carousel3d.js', ssr: false }
+    // { src: '~/plugins/bootstrap.js', ssr: false },
+    // '~/plugins/axios'
+  ],
+  css:[
+    'swiper/dist/css/swiper.css',
+    'assets/css/bootstrap.css',
+    '~/assets/css/animate.css'
+
+  ],
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+   vendor:['axios'],
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
